91a0d3fc0800b7422b3f4f471c268197fa5d8f94
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-05-12 20:23:46 +0300

    feat: add validation to first stage registartion

--- src/pages/registration/registration-form-first.tsx
+++ src/pages/registration/registration-form-first.tsx
@@ -1,3 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
 import { cn } from '@/lib/utils';
 import { Button } from '@/pages/registration/button';
 import { Input } from '@/pages/registration/input';
@@ -7,13 +11,47 @@ type Props = {
   onNext: () => void;
 };
 
+const formSchema = z
+  .object({
+    email: z.string().email('Invalid email address, (e.g., example@email.com)'),
+    password: z
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/,
+        'Must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number'
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Password do not match',
+    path: ['confirmPassword'],
+  });
+
+type FormData = z.infer<typeof formSchema>;
+
 export function RegistrationFormFirst({
   onNext,
   className,
   ...props
 }: React.ComponentPropsWithoutRef<'form'> & Props): React.JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
+  const onSubmit = (data: FormData) => {
+    console.log('form data submitted', data);
+    onNext();
+  };
   return (
-    <form className={cn('flex flex-col gap-6', className)} {...props}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={cn('flex flex-col gap-6', className)}
+      {...props}
+    >
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold capitalize">Get started now</h1>
         <p className="text-balance text-sm text-muted-foreground">
@@ -23,7 +61,16 @@ export function RegistrationFormFirst({
       <div className="grid gap-6">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="Email" required />
+          <Input
+            id="email"
+            type="email"
+            placeholder="Email"
+            {...register('email')}
+            required
+          />
+          {errors.email && (
+            <p className="text-sm text-red-500">{errors.email.message}</p>
+          )}
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
@@ -34,7 +81,11 @@ export function RegistrationFormFirst({
             type="password"
             placeholder="Password"
             required
+            {...register('password')}
           />
+          {errors.password && (
+            <p className="text-sm text-red-500">{errors.password.message}</p>
+          )}
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
@@ -45,9 +96,15 @@ export function RegistrationFormFirst({
             type="password"
             placeholder="Repeat password"
             required
+            {...register('confirmPassword')}
           />
+          {errors.confirmPassword && (
+            <p className="text-sm text-red-500">
+              {errors.confirmPassword.message}
+            </p>
+          )}
         </div>
-        <Button type="button" className="w-full" onClick={onNext}>
+        <Button type="submit" className="w-full">
           Next
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
