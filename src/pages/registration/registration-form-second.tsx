515044e49e2d504bef358d2927e8dbe1fdd5f68e
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-05-13 10:51:11 +0300

    feat: add validation to second stage registartion form

--- src/pages/registration/registration-form-second.tsx
+++ src/pages/registration/registration-form-second.tsx
@@ -14,22 +14,51 @@ type Props = {
 };
 
 const FormSchema = z.object({
-  dob: z.date({
-    required_error: 'A date of birth is required.',
-  }),
+  name: z.string().min(1, 'Name must be at least 1 character'),
+  lastName: z.string().min(1, 'Last name must be at least 1 character'),
+  dob: z
+    .date({
+      required_error: 'A date of birth is required.',
+    })
+    .refine(
+      (dob) => {
+        const today = new Date();
+        const thirteenYearsAgo = new Date(
+          today.getFullYear() - 13,
+          today.getMonth(),
+          today.getDate()
+        );
+        return dob <= thirteenYearsAgo;
+      },
+      { message: 'You must be at least 13 years old' }
+    ),
 });
+type FormData = z.infer<typeof FormSchema>;
+
 export function RegistrationFormSecond({
   onNext,
   className,
   ...props
 }: React.ComponentPropsWithoutRef<'form'> & Props): React.JSX.Element {
-  const form = useForm<z.infer<typeof FormSchema>>({
+  const form = useForm<FormData>({
     resolver: zodResolver(FormSchema),
   });
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+  const onSubmit = (data: FormData) => {
+    console.log('form data submitted', data);
+    onNext();
+  };
   return (
     <Form {...form}>
-      <form className={cn('flex flex-col gap-6', className)} {...props}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={cn('flex flex-col gap-6', className)}
+        {...props}
+      >
         <div className="flex flex-col items-center gap-2 text-center">
           <h1 className="text-2xl font-bold capitalize">Registration</h1>
           <p className="text-balance text-sm text-muted-foreground">
@@ -39,7 +68,16 @@ export function RegistrationFormSecond({
         <div className="grid gap-6">
           <div className="grid gap-2">
             <Label htmlFor="name">Name</Label>
-            <Input id="name" type="text" placeholder="Name" required />
+            <Input
+              id="name"
+              type="text"
+              placeholder="Name"
+              {...register('name')}
+              required
+            />
+            {errors.name && (
+              <p className="text-sm text-red-500">{errors.name.message}</p>
+            )}
           </div>
           <div className="grid gap-2">
             <div className="flex items-center">
@@ -49,14 +87,18 @@ export function RegistrationFormSecond({
               id="last-name"
               type="text"
               placeholder="Last name"
+              {...register('lastName')}
               required
             />
+            {errors.lastName && (
+              <p className="text-sm text-red-500">{errors.lastName.message}</p>
+            )}
           </div>
           <div className="grid gap-2">
             <Label htmlFor="name">Birthday</Label>
             <BirthdayCalendar form={form.control} />
           </div>
-          <Button type="button" className="w-full" onClick={onNext}>
+          <Button type="submit" className="w-full">
             Next
           </Button>
           <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
