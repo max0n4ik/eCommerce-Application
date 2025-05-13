586f04adafc4f2e56e2d0e8a8089c159d57cbbf5
max0n4ik <[max2004.gorohov@yandex.ru]>
2025-05-13 20:24:12 +0300

    fix: scheme exporting

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -6,19 +6,8 @@ import { z } from 'zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { authenticate } from '@/lib/actions';
+import { authenticate, schema } from '@/lib/actions';
 import { cn } from '@/lib/utils';
-
-const schema = z.object({
-  email: z.string().email('Invalid email address, (e.g., example@email.com)'),
-  password: z
-    .string()
-    .min(8, 'Password must be at least 8 characters')
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/,
-      'Must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number'
-    ),
-});
 
 export function LoginForm({
   className,
--- src/lib/actions.tsx
+++ src/lib/actions.tsx
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const schema = z.object({
+export const schema = z.object({
   email: z.string().email('Invalid email address, (e.g., example@email.com)'),
   password: z
     .string()
