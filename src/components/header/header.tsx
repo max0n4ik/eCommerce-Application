f9035d7dab6d1a8b4ba1e7eaa544efe7bc57b70f
m-bond91 <ritabond1991@gmail.com>
2025-05-16 03:20:07 +0300

    feat(RSS-ECOMM-2_60): add login and registration links in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ export default function Header(): React.JSX.Element {
   return (
     <>
       <header>
-        <nav className="mb-4">
+        <nav className="mb-4 flex gap-4">
           <Link to={ROUTES.HOME}>Home</Link>
           <Link to={ROUTES.CART}>Cart</Link>
           <Link to={ROUTES.LOGIN}>Login</Link>
