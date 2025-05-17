9005251691016ecf78bc1b142833b584bd905b40
max0n4ik <[max2004.gorohov@yandex.ru]>
2025-05-17 23:04:49 +0300

    fix: remove comments

--- src/services/build-client.ts
+++ src/services/build-client.ts
@@ -1,13 +1,12 @@
 import {
   ClientBuilder,
-  type AuthMiddlewareOptions, // Required for auth
-  type HttpMiddlewareOptions, // Required for sending HTTP requests
+  type AuthMiddlewareOptions,
+  type HttpMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
 
 const projectKey = `${import.meta.env.VITE_PROJECT_KEY}`;
 const scopes = [`${import.meta.env.VITE_SCOPES}`];
 
-// Configure authMiddlewareOptions
 const authMiddlewareOptions: AuthMiddlewareOptions = {
   host: `${import.meta.env.VITE_AUTH_URL}`,
   projectKey: projectKey,
@@ -19,14 +18,12 @@ const authMiddlewareOptions: AuthMiddlewareOptions = {
   fetch,
 };
 
-// Configure httpMiddlewareOptions
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
   host: `${import.meta.env.VITE_API_URL}`,
   fetch,
 };
 
-// Export the ClientBuilder
 export const ctpClient = new ClientBuilder()
   .withClientCredentialsFlow(authMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions) // Include middleware for logging
+  .withHttpMiddleware(httpMiddlewareOptions)
   .build();
