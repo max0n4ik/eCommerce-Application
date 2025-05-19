45f7e59bd0f83ad34b62d780c2165df5ee954bb5
max0n4ik <[max2004.gorohov@yandex.ru]>
2025-05-19 21:39:31 +0300

    feat: add local storage integration

--- src/store/login.ts
+++ src/store/login.ts
@@ -1,13 +1,21 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface AuthState {
   isAuth: boolean;
   setIsAuth: (value: boolean) => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  isAuth: false,
-  setIsAuth: (value: boolean): void => set({ isAuth: value }),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      isAuth: false,
+      setIsAuth: (value: boolean): void => set({ isAuth: value }),
+    }),
+    {
+      name: 'auth-storage',
+    }
+  )
+);
 
 export const authStore = useAuthStore.getState();
