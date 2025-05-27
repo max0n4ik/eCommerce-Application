8a685de575ca20191b26784f4dec55853510a979
max0n4ik <[max2004.gorohov@yandex.ru]>
2025-05-27 19:51:53 +0300

    fix: remove category in card

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,28 +1,15 @@
-import { useEffect } from 'react';
-
-import useCatalogStore from '@/store/catalog';
-import {
-  formatPrice,
-  getDiscountedPrice,
-  getCategoryName,
-} from '@/utils/catalog';
+import { formatPrice, getDiscountedPrice } from '@/utils/catalog';
 import type { ProductCardI } from '@/utils/interfaces';
 
 export default function ProductCard({
   imageUrl,
   imageAlt,
   id,
-  category,
   price,
   permyriad,
   name,
   description,
 }: ProductCardI): React.JSX.Element {
-  const { categories, fetchCategories } = useCatalogStore();
-  useEffect(() => {
-    fetchCategories();
-  }, [fetchCategories]);
-
   return (
     <div key={id} className="border p-4 rounded shadow">
       {imageUrl ? (
@@ -35,11 +22,6 @@ export default function ProductCard({
         <div className="h-40 bg-gray-100 mb-2 rounded" />
       )}
       <div className="flex flex-col gap-2">
-        <p className="text-sm text-gray-600">
-          {category && category.length > 0
-            ? getCategoryName(category[0].id, categories)
-            : 'Без категории'}
-        </p>
         <h3 className="font-bold text-lg">{name}</h3>
         <h5 className="line-clamp-2">{description?.en}</h5>
         <div className="flex gap-2 justify-end">
