d19277a28b435cfc320a18e036b529c02749ff3a
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-05-28 18:43:33 +0300

    feat: add clear loading render

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -14,7 +14,11 @@ export default function Product(): React.JSX.Element {
   }, [id, fetchProduct]);
 
   if (productLoading) {
-    return <div>Product loading ...</div>;
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 bg-primary"></div>
+      </div>
+    );
   }
   if (error) {
     return (
