3a137de958d790e3969647679bc3e5e46ccc5470
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-05-29 12:55:37 +0300

    refactor: refactor cards styles

--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -33,7 +33,7 @@ export default function Catalog(): React.JSX.Element {
     <div>
       <div className="hero"></div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
         {products.map((p: ProductCardI) => (
           <ProductCard key={p.id} {...p} />
         ))}
