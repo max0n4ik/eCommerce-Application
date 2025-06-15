51dfb99576de21d675142bd04b27dbe8220f60f7
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-06-15 21:04:46 +0300

    feat: add pagination to catalog page

--- src/components/catalog/catalog-products.tsx
+++ src/components/catalog/catalog-products.tsx
@@ -1,4 +1,9 @@
+import { useEffect, useMemo, useState } from 'react';
+
+import { Button } from '../ui/button';
+
 import { ProductCard } from '@/components/product-card';
+import { ITEMS_PER_PAGE } from '@/utils/constantes';
 import type { ProductCardI } from '@/utils/interfaces';
 
 interface CatalogProductsProps {
@@ -10,12 +15,69 @@ export function CatalogProducts({
   products,
   filteredProductIds,
 }: CatalogProductsProps): React.JSX.Element {
+  const [currentPage, setCurrentPage] = useState(1);
+  const filteredProducts = useMemo(
+    () =>
+      filteredProductIds
+        .map((filtered) => products.find((p) => p.id === filtered.id))
+        .filter((p): p is ProductCardI => !!p),
+    [filteredProductIds, products]
+  );
+  const totalPages = Math.ceil(filteredProducts.length / ITEMS_PER_PAGE);
+  const paginatedProducts = filteredProducts.slice(
+    (currentPage - 1) * ITEMS_PER_PAGE,
+    currentPage * ITEMS_PER_PAGE
+  );
+  const handlePageChange = (newPage: number): void => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+  useEffect(() => {
+    const productContainer = document.querySelector(
+      '#catalog-controls-container'
+    );
+    if (productContainer) {
+      productContainer.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [currentPage]);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-      {filteredProductIds?.map((filteredProduct) => {
-        const product = products.find((p) => p.id === filteredProduct.id);
-        return product ? <ProductCard key={product.id} {...product} /> : null;
-      })}
+    <div id="product-container" className="flex flex-col gap-6 p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {paginatedProducts.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+
+      {totalPages > 1 && (
+        <div className="flex justify-center items-center gap-2">
+          <Button
+            variant="outline"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Back
+          </Button>
+
+          {Array.from({ length: totalPages }, (_, i) => (
+            <Button
+              key={i + 1}
+              variant={currentPage === i + 1 ? 'default' : 'outline'}
+              onClick={() => handlePageChange(i + 1)}
+            >
+              {i + 1}
+            </Button>
+          ))}
+
+          <Button
+            variant="outline"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
